149e37884dfb7793f55d26d13aaf54087806a243
Jim <jbenkejr@gmail.com>
2019-11-18 08:35:07 -0500

    CoffeeShopLab

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayUser(RegisterUser newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(newUser);
+            }
+            else
+            {
+                return View("AddUser");
+            }
+        }
+    }
+}
---
+++ Models/RegisterUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18_CoffeeShop.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Not a Valid Name!")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Not a Valid Name!")]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        [RegularExpression(@"[a-zA-Z0-9]{3,30}@[A-Za-z]{3,10}\.[A-Za-z]{2,3}$", ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 5, ErrorMessage= "Invalid Username")]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", ErrorMessage= "Invalid Password!")]
+        public string PassWord { get; set; }
+
+    }
+}
